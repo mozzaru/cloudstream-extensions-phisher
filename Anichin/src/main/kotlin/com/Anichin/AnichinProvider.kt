8a88481b7d843d8f8b006ac5099a5b075046ab57
mozzaru <rianyanuar52@gmail.com>
2025-06-09 22:51:11 +0700

    fix

--- Anichin/src/main/kotlin/com/Anichin/AnichinProvider.kt
+++ Anichin/src/main/kotlin/com/Anichin/AnichinProvider.kt
@@ -3,7 +3,10 @@ package com.Anichin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.extractors.Dailymotion
-//import com.lagradost.cloudstream3.extractors.Okrulink
+import com.lagradost.cloudstream3.extractors.Odnoklassniki
+import com.lagradost.cloudstream3.extractors.OkRuHTTP
+import com.lagradost.cloudstream3.extractors.OkRuSSL
+import com.lagradost.cloudstream3.extractors.Okrulink
 import com.Anichin.extractors.*
 //import com.Anichin.extractors.Okru
 //import com.Anichin.extractors.Rumble
@@ -16,7 +19,10 @@ class AnichinProvider: BasePlugin() {
     override fun load() {
         registerMainAPI(Anichin())
         registerExtractorAPI(Dailymotion())
-        //registerExtractorAPI(Okru())
+        registerExtractorAPI(Okrulink())
+        registerExtractorAPI(OkRuSSL())
+        registerExtractorAPI(OkRuHTTP())
+        registerExtractorAPI(Odnoklassniki())
         //registerExtractorAPI(Rumble())
         registerExtractorAPI(StreamRuby())
         //registerExtractorAPI(VidGuard())
