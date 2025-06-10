3c1f2ad3482dda8da74911bf3c35de959aec23a2
mozzaru <rianyanuar52@gmail.com>
2025-06-11 00:14:14 +0700

    fix extractors rumble

--- Anichin/src/main/kotlin/com/Anichin/ExtractorsRumble.kt
+++ Anichin/src/main/kotlin/com/Anichin/ExtractorsRumble.kt
@@ -25,29 +25,27 @@ class Rumble : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val response = app.get(
-            url, referer = referer ?: "$mainUrl/"
-        )
+        val response = app.get(url, referer = referer ?: "$mainUrl/")
 
         val playerScript = response.document
             .selectFirst("script:containsData(mp4)")?.data()
-            ?.substringAfter("{\"mp4")?.substringBefore("\"evt\":{") ?: ""
+            ?.substringAfter("{\"mp4")?.substringBefore("\"evt\":{") ?: return
 
-        val regex = """"url":"(.*?)"|h":(.*?)\}""".toRegex()
+        val regex = """"url":"(https[^"]+)"""".toRegex()
         val matches = regex.findAll(playerScript)
 
         for (match in matches) {
             val href = match.groupValues[1].replace("\\/", "/")
+
             val qualityValue = Regex("(\\d{3,4})").find(href)?.value?.toIntOrNull()
 
-            // ✅ Hanya tampilkan kualitas yang wajar (contoh: 240p, 360p, 480p, 720p, 1080p)
             if (qualityValue != null && qualityValue in listOf(240, 360, 480, 720, 1080)) {
                 val quality = "${qualityValue}p"
 
                 callback.invoke(
                     newExtractorLink(
                         source = this.name,
-                        name = quality, // Tampilkan kualitas saja tanpa 'Rumble'
+                        name = quality,
                         url = href,
                         type = INFER_TYPE
                     ) {
