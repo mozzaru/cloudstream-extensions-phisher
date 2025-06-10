b8da57346f6fc88bbdf1ca61ff952a2799364be6
mozzaru <rianyanuar52@gmail.com>
2025-06-10 22:01:28 +0700

    extractors rumble

--- Anichin/src/main/kotlin/com/Anichin/ExtractorsRumble.kt
+++ Anichin/src/main/kotlin/com/Anichin/ExtractorsRumble.kt
@@ -1,10 +1,17 @@
 package com.Anichin
 
-import com.lagradost.cloudstream3.app 
-import com.lagradost.cloudstream3.utils.ExtractorApi 
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.newExtractorLink
+import android.util.Log
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.ErrorLoadingException
 import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.INFER_TYPE
+import com.lagradost.cloudstream3.utils.getQualityFromName
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.*
 
 class Rumble : ExtractorApi() {
@@ -21,22 +28,28 @@ class Rumble : ExtractorApi() {
         val response = app.get(
             url, referer = referer ?: "$mainUrl/"
         )
-        val playerScript =
-            response.document.selectFirst("script:containsData(mp4)")?.data()
-                ?.substringAfter("{\"mp4")?.substringBefore("\"evt\":{") ?:""
+
+        val playerScript = response.document
+            .selectFirst("script:containsData(mp4)")?.data()
+            ?.substringAfter("{\"mp4")?.substringBefore("\"evt\":{") ?: ""
+
         val regex = """"url":"(.*?)"|h":(.*?)\}""".toRegex()
         val matches = regex.findAll(playerScript)
+
         for (match in matches) {
             val href = match.groupValues[1].replace("\\/", "/")
+            val quality = "720p" // Default quality; or extract it based on context if available
+
             callback.invoke(
                 newExtractorLink(
-                    name,
-                    name,
+                    source = this.name,
+                    name = this.name,
                     url = href,
-                    INFER_TYPE
+                    type = INFER_TYPE
                 ) {
-                    this.referer = ""
-                    this.quality = getQualityFromName("")
+                    this.referer = "$mainUrl/"
+                    this.quality = getQualityFromName(quality)
+                    this.headers = headers
                 }
             )
         }
