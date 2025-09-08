aa3c1401bf0519760985e9264124faa805e68adc
mozzaru <100791931+mozzaru@users.noreply.github.com>
2025-09-08 09:36:57 +0700

    Update Anichin.kt

--- Anichin/src/main/kotlin/com/Anichin/Anichin.kt
+++ Anichin/src/main/kotlin/com/Anichin/Anichin.kt
@@ -1,9 +1,12 @@
 package com.Anichin
 
-import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.network.CloudflareKiller
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 class Anichin : MainAPI() {
     override var mainUrl              = "https://anichin.moe"
@@ -11,7 +14,13 @@ class Anichin : MainAPI() {
     override val hasMainPage          = true
     override var lang                 = "id"
     override val hasDownloadSupport   = true
-    override val supportedTypes       = setOf(TvType.Movie,TvType.Anime)
+    override val supportedTypes       = setOf(TvType.Movie, TvType.Anime)
+
+    private val cloudflareClient: OkHttpClient by lazy {
+        app.baseClient.newBuilder()
+            .addInterceptor(CloudflareKiller())
+            .build()
+    }
 
     override val mainPage = mainPageOf(
         "anime/?order=update" to "Rilisan Terbaru",
@@ -22,13 +31,13 @@ class Anichin : MainAPI() {
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl/${request.data}&page=$page").document
-        val home     = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
+        val document = cloudflareClient.get("$mainUrl/${request.data}&page=$page").document
+        val home = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
 
         return newHomePageResponse(
-            list    = HomePageList(
-                name               = request.name,
-                list               = home,
+            list = HomePageList(
+                name = request.name,
+                list = home,
                 isHorizontalImages = false
             ),
             hasNext = true
@@ -36,20 +45,19 @@ class Anichin : MainAPI() {
     }
 
     private fun Element.toSearchResult(): SearchResponse {
-        val title     = this.select("div.bsx > a").attr("title")
-        val href      = fixUrl(this.select("div.bsx > a").attr("href"))
+        val title = this.select("div.bsx > a").attr("title")
+        val href = fixUrl(this.select("div.bsx > a").attr("href"))
         val posterUrl = fixUrlNull(this.select("div.bsx > a img").attr("src"))
         return newMovieSearchResponse(title, href, TvType.Movie) {
             this.posterUrl = posterUrl
         }
     }
 
-
     override suspend fun search(query: String): List<SearchResponse> {
         val searchResponse = mutableListOf<SearchResponse>()
 
         for (i in 1..3) {
-            val document = app.get("${mainUrl}/page/$i/?s=$query").document
+            val document = cloudflareClient.get("$mainUrl/page/$i/?s=$query").document
 
             val results = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
 
@@ -65,41 +73,41 @@ class Anichin : MainAPI() {
         return searchResponse
     }
 
-    @Suppress("SuspiciousIndentation")
     override suspend fun load(url: String): LoadResponse {
-        val document = app.get(url).document
-        val title       = document.selectFirst("h1.entry-title")?.text()?.trim().toString()
-        val href=document.selectFirst(".eplister li > a")?.attr("href") ?:""
+        val document = cloudflareClient.get(url).document
+        val title = document.selectFirst("h1.entry-title")?.text()?.trim().orEmpty()
+        val href = document.selectFirst(".eplister li > a")?.attr("href").orEmpty()
         var poster = document.select("div.ime > img").attr("src")
         val description = document.selectFirst("div.entry-content")?.text()?.trim()
-        val type=document.selectFirst(".spe")?.text().toString()
-        val tvtag=if (type.contains("Movie")) TvType.Movie else TvType.TvSeries
+        val type = document.selectFirst(".spe")?.text().orEmpty()
+        val tvtag = if (type.contains("Movie", ignoreCase = true)) TvType.Movie else TvType.TvSeries
+
         return if (tvtag == TvType.TvSeries) {
-            val Eppage= document.selectFirst(".eplister li > a")?.attr("href") ?:""
-            val doc= app.get(Eppage).document
-            val episodes=doc.select("div.episodelist > ul > li").map { info->
-                        val href1 = info.select("a").attr("href")
-                        val episode = info.select("a span").text().substringAfter("-").substringBeforeLast("-")
-                        val posterr=info.selectFirst("a img")?.attr("src") ?:""
-                        newEpisode(href1)
-                        {
-                            this.name=episode
-                            this.posterUrl=posterr
-                        }
+            val Eppage = document.selectFirst(".eplister li > a")?.attr("href").orEmpty()
+            val doc = cloudflareClient.get(Eppage).document
+            val episodes = doc.select("div.episodelist > ul > li").map { info ->
+                val href1 = info.select("a").attr("href")
+                val episode = info.select("a span").text().substringAfter("-").substringBeforeLast("-")
+                val posterr = info.selectFirst("a img")?.attr("src").orEmpty()
+                newEpisode(href1) {
+                    this.name = episode
+                    this.posterUrl = posterr
+                }
             }
-            if (poster.isEmpty())
-            {
-                poster=document.selectFirst("meta[property=og:image]")?.attr("content")?.trim().toString()
+
+            if (poster.isEmpty()) {
+                poster = document.selectFirst("meta[property=og:image]")?.attr("content").orEmpty()
             }
+
             newTvSeriesLoadResponse(title, url, TvType.Anime, episodes.reversed()) {
                 this.posterUrl = poster
                 this.plot = description
             }
         } else {
-            if (poster.isEmpty())
-            {
-                poster=document.selectFirst("meta[property=og:image]")?.attr("content")?.trim().toString()
+            if (poster.isEmpty()) {
+                poster = document.selectFirst("meta[property=og:image]")?.attr("content").orEmpty()
             }
+
             newMovieLoadResponse(title, url, TvType.Movie, href) {
                 this.posterUrl = poster
                 this.plot = description
@@ -107,15 +115,20 @@ class Anichin : MainAPI() {
         }
     }
 
-    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        val document = app.get(data).document
-        document.select(".mobius option").forEach { server->
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = cloudflareClient.get(data).document
+        document.select(".mobius option").forEach { server ->
             val base64 = server.attr("value")
-            val decoded=base64Decode(base64)
+            val decoded = base64Decode(base64)
             val doc = Jsoup.parse(decoded)
-            val href=doc.select("iframe").attr("src")
+            val href = doc.select("iframe").attr("src")
             val url = fixUrl(href)
-            loadExtractor(url,subtitleCallback, callback)
+            loadExtractor(url, subtitleCallback, callback)
         }
         return true
     }
